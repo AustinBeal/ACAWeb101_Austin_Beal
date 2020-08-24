923a6d390c97031d2daa272b555c2083ab18b99b
AustinBeal <austinbeal89@gmail.com>
2020-08-23 20:28:25 -0500

    finished animations!

---
+++ interactive-resume.js
@@ -0,0 +1,8 @@
+function disapearingAct() {
+  let email = document.getElementById('contactemail')
+  onclick = email.classList.toggle('show')
+}
+function disapearingImg() {
+  let portrait = document.getElementById('isa-prof')
+  onclick = portrait.classList.toggle('gone')
+}
